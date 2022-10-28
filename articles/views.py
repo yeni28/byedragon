605697989e41272fb48354037148462bf9c4cc60
yen <suntofu0228@naver.com>
2022-10-28 15:22:54 +0900

    viewsup

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
+
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles' : articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        "article": article,
+    }
+    return render(request, "articles/detail.html", context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect("articles:index")
