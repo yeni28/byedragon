5433a836abae5837f0c0e0f732248650a2741387
choihyoingwoon <cpobo@hanmail.net>
2022-10-28 15:25:59 +0900

    view

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from .models import Article
 from .forms import ArticleForm
 
@@ -10,6 +13,43 @@ def index(request):
         'articles' : articles,
     }
     return render(request, 'articles/index.html', context)
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.user == article.user:
+        if request.method == 'POST':
+            form = ArticleForm(request.POST, instance=article)
+            if form.is_valid():
+                form.save()
+                return redirect('articles:detail', article.pk)
+        else:
+            form = ArticleForm(instance=article)
+    else:
+        return redirect('articles:index')
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
 
 
 def detail(request, pk):
